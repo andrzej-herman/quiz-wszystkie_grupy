4f1026ac6e869bb59f89ca2419f7876c1cf7f4e2
andrzej-herman <andrzejek.herman@gmail.com>
2020-11-17 14:17:27 +0100

    Dodałem pierwszy kod

---
+++ Quiz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Program
+    {
+        // zapewne jak się domyślacie ten kod będzie ulegał zmianie w trakcie naszej nauki i pisania Quizu
+        // Na razie przedstawia się tak:
+
+
+        static void Main(string[] args)
+        {
+            WyswietlTytulQuizu("Andrzej Herman");
+            Console.ReadLine();
+        }
+
+
+
+        // metoda, która wyswietla czołówkę naszego Quizu => żeby był większy bajer :)
+        static void WyswietlTytulQuizu(string imie_i_nazwisko_autora)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            W("*************************************************************************");
+            W();
+            W("                                QUIZ                                     ");
+            W($"                       Autor: {imie_i_nazwisko_autora}                  ");
+            W();
+            W("*************************************************************************");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        // skróciłem sobie, żeby nie musieć ciągle pisać: Console.WriteLine ....
+        static void W(string text = null)
+        {
+            Console.WriteLine(text);
+        }
+
+
+    }
+}
---
+++ Quiz/Pytanie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    public class Pytanie
+    {
+        // zapewne jak się domyślacie ten kod będzie ulegał zmianie w trakcie naszej nauki i pisania Quizu
+        // Na razie przedstawia się tak:
+
+
+        // treść pytania
+        public string Tresc { get; set; }
+
+        // kategoria pytania wyrażona jako liczba (przedstawia kwotę za jaką jest pytanie)
+        // sprawdziłem => w milionerach jest 12 kategorii pytań, od 500 zł do 1 000 000
+        public int Kategoria { get; set; }
+
+        // Cztery odpowiedzi
+        public string Odpowiedz_1 { get; set; }
+        public string Odpowiedz_2 { get; set; }
+        public string Odpowiedz_3 { get; set; }
+        public string Odpowiedz_4 { get; set; }
+    }
+}
